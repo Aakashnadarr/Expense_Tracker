38e952601bc22c9155903eb343bd3dd184b2d385
Aakash Nadar <aakashnadar2005@gmail.com>
2025-06-14 13:08:20 +0530

    Expense tracker updated

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('register/', register, name='register'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
     path('delete/<uuid:uuid>/', deletetransaction, name='deletetransaction'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,66 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from app.models import *
 from django.db.models import Sum
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db.models import Q
 
+def register(request):
+   if request.method == 'POST':
+       first_name = request.POST.get('first_name')
+       last_name = request.POST.get('last_name')
+       email = request.POST.get('email')
+       username = request.POST.get('username')
+       password = request.POST.get('password')
+       user_object= User.objects.filter(
+           Q(username=username) | Q(email=email)
+       )
+       
+       if user_object.exists():
+           messages.error(request, 'Error:Username or Email already exists')
+           return redirect('/register/')
+       user_object = User.objects.create_user(
+           first_name=first_name,
+           last_name=last_name,
+           email=email,
+           username=username,
+           
+       )
+       user_object.set_password(password)
+       user_object.save()
+       messages.success(request, 'Success:Account created successfully')
+       return redirect('/login/')
+   else:
+       return render(request, 'register.html')   
+def login_page(request):
+        
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user_object = User.objects.filter(
+                username=username,
+            ) 
+            if not user_object.exists():
+                messages.error(request, 'Error:Username does not exist')
+                return redirect('/login/')
+            user_object = authenticate(
+                username=username,
+                password=password
+            )
+            if not user_object:
+                messages.error(request, 'Error:Invalid credentials')
+                return redirect('/login/')
+            
+            login(request, user_object)
+            messages.success(request, 'Success:Logged in successfully')
+            return redirect('/')
+        else:
+            return render(request, 'login.html')
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'Success:Logged out successfully')
+    return redirect('/login/')
+        
 def index(request):
     if request.method == 'POST':
         print(request.POST)
@@ -13,7 +72,8 @@ def index(request):
         description = request.POST.get('description')
         if description.strip() == '':
             messages.info(request, 'Description is required')
-            return redirect('/') 
+            return redirect('/')
+         
         
         
         Transaction.objects.create(amount=amount, description=description)
